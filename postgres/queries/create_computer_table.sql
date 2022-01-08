edf72ebd316946b00e05ea466ee8c6072ec3e885
Gustavo Akashi <gustavo.akashi@quintoandar.com.br>
2022-01-08 01:05:15 -0300

    Changing column datatype

--- postgres/queries/create_computer_table.sql
+++ postgres/queries/create_computer_table.sql
@@ -1,11 +1,12 @@
 create table computadores (
 	ram_name VARCHAR(100),
+	ram_rate INTEGER,
 	video_name VARCHAR(100),
 	video_rate INTEGER,
 	motherboard_name VARCHAR(100),
 	processor_name VARCHAR(100),
 	processor_rate INTEGER,
-	price REAL,
+	price NUMERIC,
 	computer_name VARCHAR(255),
 	storage_name VARCHAR(255),
 	storage_type INTEGER,
